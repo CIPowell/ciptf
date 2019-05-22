aae91e0b986e3592849b6b185f3c4e9840dc468d
Chris Powell <chris.powell@charlottetilbury.com>
2019-05-22 08:04:01 +0100

    Test commit

--- codestore.tf
+++ codestore.tf
@@ -1,3 +1,10 @@
+resource "github_repository" "sentinels" {
+  name  = "sentinels"
+  description = "An example github project"
+
+  private = true
+}
+
 resource "aws_s3_bucket" "lambda_functions_dev" {
   bucket = "cip-lambda-store-dev"
   acl    = "private"
