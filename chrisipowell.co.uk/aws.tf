f12db74621a1b4a680ee5a174971f122e969066f
Chris I Powell <chris@chrisipowell.co.uk>
2023-05-07 08:12:11 +0100

    Add Google Site Verification record

--- chrisipowell.co.uk/aws.tf
+++ chrisipowell.co.uk/aws.tf
@@ -5,3 +5,13 @@ provider "aws" {
 resource "aws_route53_zone" "chrisipowell_co_uk" {
     name = "chrisipowell.co.uk."
 }
+
+resource "aws_route53_record" "google_search_console_verification" {
+  zone_id = aws_route53_zone.chrisipowell_co_uk
+  name = "@"
+  ttl = 600
+  type = "TXT"
+  records = [  
+    "google-site-verification=QVqHNkgfg3NykWtYY453hHrLg_J76Att6rl8fm1pVpA"
+  ]
+}
